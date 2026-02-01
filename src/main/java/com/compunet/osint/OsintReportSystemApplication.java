e9cc6fddcfd36a2f8e85ee540a1f30055c8f7326
BvnoV <he.valenzuelaf@duocuc.cl>
2026-01-31 22:43:54 -0300

    Inicio del Proyecto

---
+++ src/main/java/com/compunet/osint/OsintReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.compunet.osint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsintReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsintReportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compunet/osint/OsintReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compunet.osint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsintReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
